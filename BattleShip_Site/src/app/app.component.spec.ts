3d210feec0b762fbed2ff6a70b6bfc0d578d6459
ethanhutchins33 <ethanhutchins33@gmail.com>
2022-03-10 15:44:36 +0000

    updated to new version of angluar. re-initialised project.

--- BattleShip_Site/src/app/app.component.spec.ts
+++ BattleShip_Site/src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Battleship'`, () => {
+  it(`should have as title 'BattleShip_Site'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Battleship');
+    expect(app.title).toEqual('BattleShip_Site');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Battleship app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('BattleShip_Site app is running!');
   });
 });
