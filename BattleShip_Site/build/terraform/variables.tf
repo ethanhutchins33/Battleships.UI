f25472eb5857ba4aeac5e002f05a7f3a922d478c
Ethan Hutchins <ethanhutchins33@gmail.com>
2022-04-01 11:42:01 +0100

    pass variables from pipeline to terraform for authentication

--- BattleShip_Site/build/terraform/variables.tf
+++ BattleShip_Site/build/terraform/variables.tf
@@ -1,2 +1,21 @@
 #define variables
 
+variable "ARM_SUBSCRIPTION_ID" {
+  type = string
+  description = "Azure Subcription ID to use for authenticating terraform while running in build pipeline"
+}
+
+variable "ARM_CLIENT_ID" {
+  type = string
+  description = "Azure Client ID to use for authenticating terraform while running in build pipeline"
+}
+
+variable "ARM_CLIENT_SECRET" {
+  type = string
+  description = "Azure Client Secret to use for authenticating terraform while running in build pipeline"
+}
+
+variable "ARM_TENANT_ID" {
+  type = string
+  description = "Azure Tenant ID of the Service Principle to use for authenticating terraform while running in build pipeline"
+}
