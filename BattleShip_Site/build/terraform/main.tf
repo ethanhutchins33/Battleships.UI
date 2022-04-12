96b50740a402b35da7393e7fe73942874e7040e4
Ethan Hutchins <ethanhutchins33@gmail.com>
2022-04-12 14:00:14 +0100

    remove dashes from storage name

--- BattleShip_Site/build/terraform/main.tf
+++ BattleShip_Site/build/terraform/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "static_storage" {
-  name                     = "battleships-static-site"
+  name                     = "battleshipsstaticsite"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_kind             = "StorageV2"
