90542068112219d7e4a54f82f1abd149f7970652
Ethan Hutchins <ethanhutchins33@gmail.com>
2022-04-12 13:37:53 +0100

    change to static storage site

--- BattleShip_Site/build/terraform/main.tf
+++ BattleShip_Site/build/terraform/main.tf
@@ -9,12 +9,18 @@ resource "azurerm_resource_group" "rg" {
   location = "westeurope"
 }
 
+resource "azurerm_storage_account" "static_storage" {
+  name                     = "battleships-static-site"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  enable_https_traffic_only = true
 
-#STATIC WEB APP
-resource "azurerm_static_site" "battleship_static_site" {
-  name                = "battleship-static-site"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  static_website {
+    index_document = "index.html"
+  }
 }
 
 #SQL SERVER
