1fba8edb5b725beb4aa1ef606268668e3b19eeee
Ethan Hutchins <ethanhutchins33@gmail.com>
2022-04-14 17:31:47 +0100

    started adding msal library and configuration. Set 'strict' to false - need to change back

--- BattleShip_Site/SPA/BattleShips/src/app/app.module.ts
+++ BattleShip_Site/SPA/BattleShips/src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+
+import { MsalModule } from '@azure/msal-angular'
+import { PublicClientApplication } from '@azure/msal-browser';
+import { Authority } from '@azure/msal-common';
+
+const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1;
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MsalModule.forRoot( new PublicClientApplication({
+      auth: {
+        clientId: 'e8c1b39e-79b1-4d52-9439-58f2fb7c2f9e', // Application (client) ID from the app registration
+        authority: 'common', // The Azure cloud instance and the app's sign-in audience (tenant ID, common, organizations, or consumers)
+        redirectUri: 'http://localhost:4200'// This is your redirect URI
+      },
+      cache: {
+        cacheLocation: 'localStorage',
+        storeAuthStateInCookie: isIE, // Set to true for Internet Explorer 11
+      }
+    }), null, null)
   ],
   providers: [],
   bootstrap: [AppComponent]
