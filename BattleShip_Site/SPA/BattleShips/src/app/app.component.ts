33b64939132d62b5e44ce8703816fa5ab6755dfc
Ethan Hutchins <ethanhutchins33@gmail.com>
2022-04-20 13:44:57 +0100

    sign in using redirects and configure conditional rendering

--- BattleShip_Site/SPA/BattleShips/src/app/app.component.ts
+++ BattleShip_Site/SPA/BattleShips/src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { MsalService, MsalBroadcastService } from '@azure/msal-angular';
+import { InteractionStatus } from '@azure/msal-browser';
+import { Subject } from 'rxjs';
+import { filter, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'BattleShips';
+  isIframe = false;
+  loginDisplay = false;
+  private readonly _destroying$ = new Subject<void>();
+
+  constructor(private broadcastService: MsalBroadcastService, private authService: MsalService) { }
+
+  ngOnInit() {
+    this.isIframe = window !== window.parent && !window.opener;
+
+    this.broadcastService.inProgress$
+      .pipe(
+        filter((status: InteractionStatus) => status === InteractionStatus.None),
+        takeUntil(this._destroying$)
+      )
+      .subscribe(() => {
+        this.setLoginDisplay();
+      })
+  }
+
+  login() {
+    this.authService.loginRedirect();
+  }
+
+  setLoginDisplay() {
+    this.loginDisplay = this.authService.instance.getAllAccounts().length > 0;
+  }
+
+  ngOnDestroy(): void {
+    this._destroying$.next(undefined);
+    this._destroying$.complete();
+  }
+
 }
--- BattleShip_Site/SPA/BattleShips/src/app/app.module.ts
+++ BattleShip_Site/SPA/BattleShips/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+import { MatButtonModule } from '@angular/material/button'
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 
-import { MsalModule } from '@azure/msal-angular'
+import { MsalModule, MsalRedirectComponent } from '@azure/msal-angular'
 import { PublicClientApplication } from '@azure/msal-browser';
-import { Authority } from '@azure/msal-common';
 
 const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1;
 
@@ -20,7 +24,11 @@ const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigato
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatListModule,
     MsalModule.forRoot( new PublicClientApplication({
       auth: {
         clientId: 'e8c1b39e-79b1-4d52-9439-58f2fb7c2f9e', // Application (client) ID from the app registration
