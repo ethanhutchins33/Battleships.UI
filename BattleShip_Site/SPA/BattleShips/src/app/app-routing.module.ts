e30563d3fb25d6afa3b2f1157171a1094564447c
Ethan Hutchins <ethanhutchins33@gmail.com>
2022-04-22 14:08:09 +0100

    set up guarding routes for profile button

--- BattleShip_Site/SPA/BattleShips/src/app/app-routing.module.ts
+++ BattleShip_Site/SPA/BattleShips/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
+import { MsalGuard } from '@azure/msal-angular';
 
 const routes: Routes = [
   {
     path: 'profile',
     component: ProfileComponent,
+    canActivate: [MsalGuard]
   },
   {
     path: '',
