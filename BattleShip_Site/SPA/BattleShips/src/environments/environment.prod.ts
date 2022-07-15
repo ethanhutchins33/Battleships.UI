e74fb39f8b6c062975aafcbc99b033e65906a58a
Ethan Hutchins <ethanhutchins33@gmail.com>
2022-07-15 14:11:58 +0100

    remove /profile

--- BattleShip_Site/SPA/BattleShips/src/environments/environment.prod.ts
+++ BattleShip_Site/SPA/BattleShips/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  backendApiUrl: 'https://battleships-profile-web-api.azurewebsites.net/',
-  redirectUri: 'https://battleshipsstaticsite.z6.web.core.windows.net/profile'
+  backendApiUrl: 'https://battleships-users-web-api.azurewebsites.net/',
+  redirectUri: 'https://battleshipsstaticsite.z6.web.core.windows.net/'
 };
