73a6e511f4937e53f6cba1fe6bc41ee634d7e841
Ethan Hutchins <ethanhutchins33@gmail.com>
2022-08-19 12:10:40 +0100

    remove slash

--- BattleShip_Site/SPA/BattleShips/src/environments/environment.prod.ts
+++ BattleShip_Site/SPA/BattleShips/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  backendApiUrl: 'https://battleships-users-web-api.azurewebsites.net/',
+  backendApiUrl: 'https://battleships-users-web-api.azurewebsites.net',
   redirectUri: 'https://battleshipsstaticsite.z6.web.core.windows.net/'
 };
