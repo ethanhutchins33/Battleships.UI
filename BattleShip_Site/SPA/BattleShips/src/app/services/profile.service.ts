606402c54f6914ad4cd8d7c9f1717450677abda8
Ethan Hutchins <ethanhutchins33@gmail.com>
2022-08-19 10:53:29 +0100

    working addprofile & getprofile locally

--- BattleShip_Site/SPA/BattleShips/src/app/services/profile.service.ts
+++ BattleShip_Site/SPA/BattleShips/src/app/services/profile.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { catchError, tap, throwError } from 'rxjs';
+import { catchError as handleError, tap, throwError } from 'rxjs';
 import { Profile } from '../models/profile.model';
 import { environment } from 'src/environments/environment';
+//import { BrowserUtils } from '@azure/msal-browser';
 
 @Injectable({
   providedIn: 'root'
@@ -14,9 +15,40 @@ export class ProfileService {
     private http: HttpClient
     ) { }
 
-  profile(id: string): Observable<Profile>{
+  getUserProfile(user: Profile): Observable<Profile>{
     console.log("Trying to get profile...");
-    return this.http.get<Profile>(`${environment.backendApiUrl}/api/profiles/${id}`);
+    return this.http.get<Profile>(`${environment.backendApiUrl}/api/profile`)
+    .pipe(
+      handleError(this.handleError('getUserProfile', user))
+    );
+  }
+
+  addUserProfile(user: Profile): Observable<Profile>{
+    console.log("Adding new user to the DB...");
+    return this.http.post<Profile>(`${environment.backendApiUrl}/api/profile`, user)
+    .pipe(
+      handleError(this.handleError('addUserProfile', user))
+    );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   *
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+   private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error);
+
+      // TODO: better job of transforming error for user consumption
+      //this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
 }
