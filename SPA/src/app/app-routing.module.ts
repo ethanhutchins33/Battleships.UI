be66341c9861c578e1ef0925fa95c82c99f3dcf7
Ethan Hutchins <ethanhutchins33@gmail.com>
2022-09-16 15:10:34 +0100

    rearrange & rename a few folders folders

--- SPA/src/app/app-routing.module.ts
+++ SPA/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home-page/home.component';
+import { ProfileComponent } from './profile-page/profile.component';
 import { MsalGuard } from '@azure/msal-angular';
 
 const routes: Routes = [
