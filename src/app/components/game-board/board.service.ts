62eb0540a152a0b806b428b458d03594c2de7bed
Ethan Hutchins <ethanhutchins33@gmail.com>
2022-09-30 10:10:14 +0100

    remove more dead code

--- src/app/components/game-board/board.service.ts
+++ src/app/components/game-board/board.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Board } from '../../models/board';
-import { Player } from '../../models/player';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +6,5 @@ import { Player } from '../../models/player';
 export class BoardService {
   
   constructor() { }
-  
-  board!: Board;
 
 }
