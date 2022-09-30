df94fd6cb6625ec44ea681f3711e36fd6d64211b
Ethan Hutchins <ethanhutchins33@gmail.com>
2022-09-30 10:09:42 +0100

    remove dead code

--- src/app/components/game-board/board.service.ts
+++ src/app/components/game-board/board.service.ts
@@ -11,41 +11,4 @@ export class BoardService {
   
   board!: Board;
 
-  public tiles2: number[] = [1, 2, 3, 4, 5, 6, 7];
-
-  createBoard(BOARD_SIZE: number = 7) : BoardService {
-    // create tiles for board
-    let tiles1!: string[];
-    for(let i=0; i < BOARD_SIZE; i++) {
-      for(let j=0; j < BOARD_SIZE; j++) {
-        tiles1[i] = 'X';
-      }
-    }
-
-    
-
-    // generate random ships for the board
-    // for (let i = 0; i < BOARD_SIZE * 2; i++) {
-    //   tiles = this.randomShips(tiles, size);
-    // }
-
-
-
-    // new player
-    //let player1: Player = ({ id: 1, score: 0 });
-    
-    // create board
-    this.board = ({tiles : tiles1});
-
-    return this;
-  }
-
-  getRandomInt(min: number, max: number) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  getBoard(): Board {
-    return this.board;
-  }
-
 }
