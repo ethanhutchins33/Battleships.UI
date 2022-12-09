0150d5ded73d125c330547c28c4ad182d92e55ca
Ethan Hutchins <ethanhutchins33@gmail.com>
2022-12-09 17:23:04 +0000

    edit prod environment config file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
   redirectUri: 'https://battleshipsstaticsite.z6.web.core.windows.net/',
-  BattleShipsUsersApiUrl: 'https://battleships-users-web-api.azurewebsites.net',
-  BattleShipsGameApiUrl: '',
+  BattleShipsApiUrl: 'https://battleships-users-web-api.azurewebsites.net',
 };
