06d3c793da33d667a4449517a30e1eba2f98318a
Ethan Hutchins <ethanhutchins33@gmail.com>
2022-12-25 16:53:56 +0000

    start adding an azure static web app to terraform and pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "static_storage" {
-  name                     = "battleshipsstaticsite"
+  name                     = "battleshipsstaticstoragesite"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_kind             = "StorageV2"
@@ -16,4 +16,11 @@ resource "azurerm_storage_account" "static_storage" {
     index_document = "index.html"
     error_404_document = "index.html"
   }
+}
+
+resource "azurerm_static_site" "static_site" {
+  name                     = "battleshipsstaticsite"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  sku_tier = "Standard"
 }
