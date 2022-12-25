a9ab9d51b220042b886eb963afb8df599947537f
Ethan Hutchins <ethanhutchins33@gmail.com>
2022-12-25 18:23:01 +0000

    update redirect uri

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  redirectUri: 'https://battleshipsstaticsite.z6.web.core.windows.net/',
-  BattleShipsApiUrl: 'https://battleships-users-web-api.azurewebsites.net',
+  redirectUri: 'https://bsstaticstorage.z6.web.core.windows.net/',
+  BattleShipsApiUrl: 'https://battleships-api.azurewebsites.net/api',
 };
