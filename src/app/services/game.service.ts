183b882b8060bef2ee5c4f5a0dbcebf7a40556d0
Ethan Hutchins <ethanhutchins33@gmail.com>
2023-01-05 16:44:39 +0000

    changes to game service

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,19 +1,25 @@
+import { ShotFiredDto } from './../requests/ShotFiredRequestDto';
 import { Injectable } from '@angular/core';
-import { cellStatus } from 'src/app/models/cellStatus';
-import { cellClickedEvent } from 'src/app/models/cellClickedEvent';
+import { FireShotResponseDto } from '../responses/FireShotResponseDto';
+import { cellLocationEvent } from 'src/app/models/cellLocationEvent';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { CreateGameResponseDto } from 'src/app/responses/CreateGameResponseDto';
 import { JoinGameResponseDto } from '../responses/JoinGameResponseDto';
+import { ActivatedRoute } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GameService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private readonly http: HttpClient,
+    private readonly route: ActivatedRoute
+  ) {}
 
   createGame(): Observable<CreateGameResponseDto> {
+    console.log('Creating a new game...');
     return this.http.post<CreateGameResponseDto>(
       `${environment.BattleShipsApiUrl}/game/create`,
       null
@@ -21,16 +27,28 @@ export class GameService {
   }
 
   joinGame(gameCode: string): Observable<JoinGameResponseDto> {
+    console.log(`Joining Game: ${gameCode}...`);
     return this.http.post<JoinGameResponseDto>(
       `${environment.BattleShipsApiUrl}/game/join/${gameCode}`,
       null
     );
   }
 
-  fireShot($event: cellClickedEvent): Observable<cellStatus> {
-    console.log('Joining Game');
-    return this.http.get<cellStatus>(
-      `${environment.BattleShipsApiUrl}/game/fire`
+  fireShot(cellShotAt: cellLocationEvent): Observable<FireShotResponseDto> {
+    console.log('Firing Shot...');
+    let gameCode = this.route.snapshot.paramMap.get('gameCode');
+    console.log(gameCode);
+
+    let shotFired: ShotFiredDto = {
+      GameCode: 'gameCode',
+      BoardId: 1,
+      ColChar: cellShotAt.Col,
+      RowNumber: cellShotAt.Row,
+    };
+
+    return this.http.post<FireShotResponseDto>(
+      `${environment.BattleShipsApiUrl}/game/fire`,
+      shotFired
     );
   }
 }
