5f3f21d5083633373a2b26bf1132e01b5383ecea
Ethan Hutchins <ethanhutchins33@gmail.com>
2023-01-05 19:11:42 +0000

    WIP drag/drop functionality half working

--- src/app/components/host-board/host-board.component.ts
+++ src/app/components/host-board/host-board.component.ts
@@ -1,3 +1,6 @@
+import { GameService } from 'src/app/services/game.service';
+import { CdkDragDrop } from '@angular/cdk/drag-drop';
+import { cellLocationEvent } from './../../models/cellLocationEvent';
 import { HostCellComponent } from '../host-cell/host-cell.component';
 import { Component, QueryList, ViewChildren } from '@angular/core';
 import { gameRow } from 'src/app/models/gameRow';
@@ -10,7 +13,7 @@ import { gameRow } from 'src/app/models/gameRow';
 export class HostBoardComponent {
   @ViewChildren(HostCellComponent) cells!: QueryList<HostCellComponent>;
 
-  constructor() {}
+  constructor(private gameService: GameService) {}
 
   board: Array<gameRow> = [
     { cells: ['A', 'B', 'C', 'D', 'E', 'F', 'G'], rowNumber: 1 },
@@ -21,4 +24,16 @@ export class HostBoardComponent {
     { cells: ['A', 'B', 'C', 'D', 'E', 'F', 'G'], rowNumber: 6 },
     { cells: ['A', 'B', 'C', 'D', 'E', 'F', 'G'], rowNumber: 7 },
   ];
+
+  onDrop($event: CdkDragDrop<cellLocationEvent>): void {
+    const tempCell = this.cells.find(
+      (cell) =>
+        cell.xCoord == $event.container.data.Col &&
+        cell.yCoord == $event.container.data.Row
+    );
+
+    if (tempCell) {
+      tempCell.cellStatus.status = 'ship';
+    }
+  }
 }
--- src/app/components/ship-selector/ship-selector.component.ts
+++ src/app/components/ship-selector/ship-selector.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ship } from 'src/app/models/ship';
 
 @Component({
   selector: 'app-ship-selector',
   templateUrl: './ship-selector.component.html',
-  styleUrls: ['./ship-selector.component.css']
+  styleUrls: ['./ship-selector.component.css'],
 })
 export class ShipSelectorComponent implements OnInit {
+  ships: ship[] = [{ size: 1 }, { size: 1 }, { size: 1 }];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
